6b649ab1565ff1a3e7dac6f4598b7bbc5fee7034
kai114615 <peter82615@yahoo.com.tw>
2024-07-26 03:17:13 +0800

    新增details.html標籤

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('about/', views.about),
+    path('about/<int:year>', views.about),
+    path('details/<uuid:product_id>', views.details)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     return HttpResponse('<h2>Myapp 首頁</h2>')
+
+def about(request, year=datetime.now().year):
+    return HttpResponse(f'<h2>About {year} </h2>')
+
+def details(request, product_id=''):
+    return HttpResponse('<h2>讀出商品編號{product_id}的商品</h2>')
