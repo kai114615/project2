d4d02599002f92e623ae6bcb445e0886c5263618
kai114615 <peter82615@yahoo.com.tw>
2024-07-26 13:15:49 +0800

    新增myapp程式

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('about/', views.about),
     path('about/<int:year>', views.about),
-    path('details/<uuid:product_id>', views.details)
+    path('details/<uuid:product_id>', views.details),
+    path('blog/<path:publish>', views.blog),
+    path('course/<slug:course_name>', views.course)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,10 +4,20 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2>Myapp 首頁</h2>')
+    # return HttpResponse('<h2>Myapp 首頁</h2>')
+    store_title = '商城首頁'
+    now = datetime.now()
+    id = 'kuytrhgefsadfhnfnhmjuiyktrjhesfdghcfuytdref'
+    return render(request, 'myapp/index.html', locals())  # locals功能可將參數自動整理成{dict}的形式來呈現
 
 def about(request, year=datetime.now().year):
     return HttpResponse(f'<h2>About {year} </h2>')
 
 def details(request, product_id=''):
     return HttpResponse('<h2>讀出商品編號{product_id}的商品</h2>')
+
+def blog(request, publish=None):
+    return HttpResponse(f'<h2>讀取{publish}的文章</h2>')
+
+def course(request, course_name=None):
+    return HttpResponse(f'<h2>課程名稱：{course_name}</h2>')
