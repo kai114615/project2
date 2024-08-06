04b3b5f8d37f33533e9282d41870dc85323c8d96
kai114615 <peter82615@yahoo.com.tw>
2024-08-06 16:06:59 +0800

    複習到20240801

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db import models, connection
+from django.db.utils import DatabaseError
 
 # Create your models here.
+class Category:
+    def all():
+
+        # 第二步 SQL語法
+        sql = 'SELECT * FROM category'
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                categories = cursor.fetchall()
+                return categories
+        except DatabaseError as e:
+            print(f"讀取資料錯誤: {e}")
+            return None
+        
+
+
+    def single():
+        pass
+
+    def create():
+        pass
+
+    def update():
+        pass
+
+    def delete():
+        pass
