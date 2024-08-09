7df2f9460bda672416de84422f99ccaa69fea7bf
kai114615 <peter82615@yahoo.com.tw>
2024-08-09 13:31:19 +0800

    SyntaxError: source code string cannot contain null bytes

--- member/models.py
+++ member/models.py
@@ -1,15 +1,15 @@
-# from django.db import models
-# from datetime import datetime
+from django.db import models
+from datetime import datetime
 
-# # Create your models here.
-# class Member(models.Model):
-#     member_id = models.AutoField(primary_key=True)
-#     member_name = models.CharField(max_length=20, unique=True)
-#     member_password = models.CharField(max_length=128)  # 密碼會加密，所以長度較長
-#     member_birth = models.DateField()
-#     member_address = models.CharField(max_length=200, null=True)
-#     last_update = models.DateTimeField(default=datetime.now())
+# Create your models here.
+class Member(models.Model):
+    member_id = models.AutoField(primary_key=True)
+    member_name = models.CharField(max_length=20, unique=True)
+    member_password = models.CharField(max_length=128)  # 密碼會加密，所以長度較長
+    member_birth = models.DateField()
+    member_address = models.CharField(max_length=200, null=True)
+    last_update = models.DateTimeField(default=datetime.now())
 
-#     class Meta:
-#         db_table = 'member'  # 指定的資料表名稱
+    class Meta:
+        db_table = 'member'  # 指定的資料表名稱
 
